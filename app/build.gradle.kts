5f4b76e25a86cf2bbe8f3d5a53107a395abeaa88
Tibor Kaszás <55442175+kaszast@users.noreply.github.com>
2026-07-05 06:18:45 +0200

    build: bump version to 0.0.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.magnifier.nwzkpq"
     minSdk = 24
     targetSdk = 35
-    versionCode = 13
-    versionName = "13.0"
+    versionCode = 14
+    versionName = "0.0.0.15"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
